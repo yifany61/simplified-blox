23f0b3891228c37f655a1549c3f47098b87ea78f
yifan_ <yifan_@node0.fifo.uw-mad-dash-pg0.wisc.cloudlab.us>
2024-02-25 21:23:06 -0600

    add cluster

--- fifo_scheduler.py
+++ fifo_scheduler.py
@@ -1,40 +1,55 @@
 import queue
 import time
 
+class ClusterState:
+    def __init__(self, num_machines=1, cpus_per_machine=4, gpus_per_machine=1, memory_per_machine=16):
+        self.machines = [{'cpus': cpus_per_machine, 'gpus': gpus_per_machine, 'memory': memory_per_machine} for _ in range(num_machines)]
+    
+    def allocate_resources(self, job_resources):
+        for machine in self.machines:
+            if all(machine[res] >= job_resources[res] for res in job_resources):
+                for res in job_resources:
+                    machine[res] -= job_resources[res]
+                return machine
+        return None
+    
+    def free_resources(self, job_resources, machine):
+        for res in job_resources:
+            machine[res] += job_resources[res]
+
 class FIFOScheduler:
-    def __init__(self):
-        # Queue to hold jobs in FIFO order
+    def __init__(self, cluster_state):
         self.job_queue = queue.Queue()
-        # Dictionary to hold job start and end times
+        self.cluster_state = cluster_state
         self.job_times = {}
-        # Track the end time of the last job to schedule the next job appropriately
         self.last_job_end_time = 0
 
     def add_job(self, job_id, job_info):
-        '''Adds a new job to the scheduler queue.'''
         self.job_queue.put((job_id, job_info))
-        print(f'Job {job_id} submitted in fifo scheduler.')
+        print(f'Job {job_id} submitted with resources {job_info["resources"]}')
 
     def schedule_jobs(self):
-        '''Schedules jobs in FIFO order and records start and end times.'''
         current_time = time.time()
-        # If there are no jobs being processed and there is at least one job waiting, schedule it
         if current_time >= self.last_job_end_time and not self.job_queue.empty():
             job_id, job_info = self.job_queue.get()
-            # Start the next job at the end time of the last job or now if no jobs have been scheduled
-            start_time = max(self.last_job_end_time, current_time)
-            end_time = start_time + job_info['duration']
-            self.last_job_end_time = end_time
-            self.job_times[job_id] = {'start_time': start_time, 'end_time': end_time}
-            print(f'Job {job_id} started at {start_time} and will end at {end_time}')
+            allocated_machine = self.cluster_state.allocate_resources(job_info['resources'])
+            if allocated_machine:
+                start_time = max(self.last_job_end_time, current_time)
+                end_time = start_time + job_info['duration']
+                self.last_job_end_time = end_time
+                self.job_times[job_id] = {'start_time': start_time, 'end_time': end_time, 'machine': allocated_machine}
+                print(f'Job {job_id} started at {start_time} and will end at {end_time}')
+                # Simulating job completion and resource deallocation
+                self.cluster_state.free_resources(job_info['resources'], allocated_machine)
+            else:
+                print(f'Job {job_id} could not be scheduled due to insufficient resources.')
 
-# Example usage
 if __name__ == '__main__':
-    scheduler = FIFOScheduler()
-    # Simulate adding jobs to the scheduler
-    scheduler.add_job(1, {'job_name': 'Job1', 'duration': 5})
-    scheduler.add_job(2, {'job_name': 'Job2', 'duration': 3})
-    # Schedule jobs and print their start and end times
+    cluster_state = ClusterState()
+    scheduler = FIFOScheduler(cluster_state)
+    # Simulate adding jobs to the scheduler with resource requirements
+    scheduler.add_job(1, {'job_name': 'Job1', 'duration': 5, 'resources': {'cpus': 2, 'gpus': 1, 'memory': 4}})
+    scheduler.add_job(2, {'job_name': 'Job2', 'duration': 3, 'resources': {'cpus': 1, 'gpus': 0, 'memory': 2}})
     start_time = time.time()
     while time.time() - start_time < 15:  # Simulate for 15 seconds
         scheduler.schedule_jobs()
--- simplified_simulator.py
+++ simplified_simulator.py
@@ -1,17 +1,17 @@
 import time
 import random
-from fifo_scheduler import FIFOScheduler 
+from fifo_scheduler import FIFOScheduler, ClusterState
 
 class SimplifiedSimulator:
-    def __init__(self, jobs_per_hour):
+    def __init__(self, jobs_per_hour, cluster_state):
         self.jobs_per_hour = jobs_per_hour
-        self.scheduler = FIFOScheduler()
+        self.scheduler = FIFOScheduler(cluster_state)
         self.job_id = 0
 
     def run_simulation(self):
-        print('Simulation started, running for 10 seconds...')
-        # Run the simulation for 10 seconds as an example
-        end_time = time.time() + 10
+        print('Simulation started, running for 1 hour...')
+        # Run the simulation for 1 hour as an example
+        end_time = time.time() + 3600
         job_generated = False
         while time.time() < end_time:
             # Ensuring at least one job is generated
@@ -19,7 +19,8 @@ class SimplifiedSimulator:
                 self.job_id += 1
                 job_info = {
                     'job_name': f'Job{self.job_id}',
-                    'duration': random.randint(100, 300)  # Random job duration between 100 and 300 seconds
+                    'duration': random.randint(100, 300),  # Random job duration between 100 and 300 seconds
+                    'resources': {'cpus': 2, 'gpus': 1, 'memory': 4}
                 }
                 self.scheduler.add_job(self.job_id, job_info)
                 print(f'Job {self.job_id} submitted (simulator).')
@@ -35,5 +36,6 @@ class SimplifiedSimulator:
 # Example usage
 if __name__ == '__main__':
     # Example: Simulate a scenario where 5 jobs are submitted per hour
-    simulator = SimplifiedSimulator(jobs_per_hour=5)
+    cluster_state = ClusterState(num_machines=10, cpus_per_machine=4, gpus_per_machine=1, memory_per_machine=16)
+    simulator = SimplifiedSimulator(jobs_per_hour=5, cluster_state=cluster_state)
     simulator.run_simulation()
